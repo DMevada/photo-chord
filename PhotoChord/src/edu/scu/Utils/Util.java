0ee82b135304708eeca2e811c0cc94fe58a28cb8
Abhiman Kolte <akolte@scu.edu>
2019-03-09 13:15:19 -0800

    Updated Node.hashCode()

--- PhotoChord/src/edu/scu/Utils/Util.java
+++ PhotoChord/src/edu/scu/Utils/Util.java
@@ -5,8 +5,6 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 import static edu.scu.Utils.Constants.sizeOfFingerTable;
@@ -40,8 +38,10 @@ public class Util
      * @return sizeOfFingerTable-bit identifier in long type
      */
     public static long hashSocketAddress(InetSocketAddress addr) {
-        int i = addr.hashCode();
-        return hashHashCode(i);
+        //int i = addr.hashCode();
+        //return hashHashCode(i);
+
+        return Integer.parseInt(addr.getHostName().substring(addr.getHostName().lastIndexOf(".")+1)) % 32;
     }
 
     /**
@@ -62,45 +62,47 @@ public class Util
      * @return sizeOfFingerTable-bit identifier in long type
      */
     private static long hashHashCode(int i) {
+//
+//        //sizeOfFingerTable bit regular hash code -> byte[4]
+//        byte[] hashbytes = new byte[4];
+//        hashbytes[0] = (byte) (i >> 24);
+//        hashbytes[1] = (byte) (i >> 16);
+//        hashbytes[2] = (byte) (i >> 8);
+//        hashbytes[3] = (byte) (i /*>> 0*/);
+//
+//        // try to create SHA1 digest
+//        MessageDigest md = null;
+//        try {
+//            md = MessageDigest.getInstance("SHA-1");
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+//
+//        // successfully created SHA1 digest
+//        // try to convert byte[4]
+//        // -> SHA1 result byte[]
+//        // -> compressed result byte[4]
+//        // -> compressed result in long type
+//        if (md != null) {
+//            md.reset();
+//            md.update(hashbytes);
+//            byte[] result = md.digest();
+//
+//            byte[] compressed = new byte[4];
+//            for (int j = 0; j < 4; j++) {
+//                byte temp = result[j];
+//                for (int k = 1; k < sizeOfFingerTable; k++) {
+//                    temp = (byte) (temp ^ result[j + k]);
+//                }
+//                compressed[j] = temp;
+//            }
+//
+//            long ret = (compressed[0] & 0xFF) << 24 | (compressed[1] & 0xFF) << 16 | (compressed[2] & 0xFF) << 8 | (compressed[3] & 0xFF);
+//            ret = ret & (long) 0xFFFFFFFFl;
+//            return ret;
+//        }
 
-        //sizeOfFingerTable bit regular hash code -> byte[4]
-        byte[] hashbytes = new byte[4];
-        hashbytes[0] = (byte) (i >> 24);
-        hashbytes[1] = (byte) (i >> 16);
-        hashbytes[2] = (byte) (i >> 8);
-        hashbytes[3] = (byte) (i /*>> 0*/);
 
-        // try to create SHA1 digest
-        MessageDigest md = null;
-        try {
-            md = MessageDigest.getInstance("SHA-1");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
-        // successfully created SHA1 digest
-        // try to convert byte[4]
-        // -> SHA1 result byte[]
-        // -> compressed result byte[4]
-        // -> compressed result in long type
-        if (md != null) {
-            md.reset();
-            md.update(hashbytes);
-            byte[] result = md.digest();
-
-            byte[] compressed = new byte[4];
-            for (int j = 0; j < 4; j++) {
-                byte temp = result[j];
-                for (int k = 1; k < sizeOfFingerTable; k++) {
-                    temp = (byte) (temp ^ result[j + k]);
-                }
-                compressed[j] = temp;
-            }
-
-            long ret = (compressed[0] & 0xFF) << 24 | (compressed[1] & 0xFF) << 16 | (compressed[2] & 0xFF) << 8 | (compressed[3] & 0xFF);
-            ret = ret & (long) 0xFFFFFFFFl;
-            return ret;
-        }
         return 0;
     }
 
